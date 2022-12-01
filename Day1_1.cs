1b03519eab57deff14785dee250d5ccea263e4d7
pandakraut <75352338+pandakraut@users.noreply.github.com>
2022-12-01 00:38:10 -0500

    Add files via upload

---
+++ Day1_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022
+{
+    class day1_1
+    {
+        static void Main(string[] args)
+        {
+
+            int maxCal = 0;
+            int curCal = 0;
+            foreach (string line in File.ReadLines("../../../inputDay1.txt"))
+            {
+
+                if (line.Trim().Length == 0)
+                {
+                    if (curCal >= maxCal)
+                    {
+                        maxCal = curCal;
+                    }
+                    curCal = 0;
+                }
+                else
+                {
+                    curCal += Convert.ToInt32(line);
+                }
+            }
+
+            Console.WriteLine("output: " + maxCal);
+        }
+    }
+}
---
+++ Day1_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022
+{
+    class day1_2
+    {
+        static void Main(string[] args)
+        {
+            Int64 curCal = 0;
+            List<Int64> maxCal = new List<Int64>();
+            foreach (string line in File.ReadLines("../../../inputDay1.txt"))
+            {
+
+                if (line.Trim().Length == 0)
+                {
+                    maxCal.Add(curCal);                    
+                    curCal = 0;
+                }
+                else
+                {
+                    curCal += Convert.ToInt64(line);
+                }
+            }
+            maxCal.Add(curCal);
+
+            maxCal.Sort();
+            Int64 total = maxCal[maxCal.Count - 1] + maxCal[maxCal.Count - 2] + maxCal[maxCal.Count - 3];
+            Console.WriteLine("output: " + total);
+        }
+    }
+}
