eb8fb1647e85359fb77f45740d2bab94599fbb35
pandakraut <75352338+pandakraut@users.noreply.github.com>
2022-12-02 18:48:13 -0500

    Add files via upload

---
+++ Day2_1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022
+{
+    class day2_1
+    {
+        static void Main(string[] args)
+        {
+
+            int totalScore = 0;
+            Dictionary<char, int> scoreMap = new Dictionary<char, int>();
+            scoreMap.Add('A', 1);
+            scoreMap.Add('B', 2);
+            scoreMap.Add('C', 3);
+            scoreMap.Add('X', 1);
+            scoreMap.Add('Y', 2);
+            scoreMap.Add('Z', 3);
+
+            Dictionary<string, int> winMap = new Dictionary<string, int>();
+            winMap.Add("A X", 3);
+            winMap.Add("A Y", 6);
+            winMap.Add("A Z", 0);
+            winMap.Add("B X", 0);
+            winMap.Add("B Y", 3);
+            winMap.Add("B Z", 6);
+            winMap.Add("C X", 6);
+            winMap.Add("C Y", 0);
+            winMap.Add("C Z", 3);
+
+            foreach (string line in File.ReadLines("../../../inputDay2.txt"))
+            {
+                totalScore += scoreMap[line[2]] + winMap[line];
+            }
+
+            Console.WriteLine("output: " + totalScore);
+        }
+    }
+}
---
+++ Day2_2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022
+{
+    class day2_2
+    {
+        static void Main(string[] args)
+        {
+
+            int totalScore = 0;
+            Dictionary<char, int> scoreMap = new Dictionary<char, int>();            
+            scoreMap.Add('X', 0);
+            scoreMap.Add('Y', 3);
+            scoreMap.Add('Z', 6);
+
+            Dictionary<string, int> winMap = new Dictionary<string, int>();
+            winMap.Add("A X", 3);
+            winMap.Add("A Y", 1);
+            winMap.Add("A Z", 2);
+            winMap.Add("B X", 1);
+            winMap.Add("B Y", 2);
+            winMap.Add("B Z", 3);
+            winMap.Add("C X", 2);
+            winMap.Add("C Y", 3);
+            winMap.Add("C Z", 1);
+
+            foreach (string line in File.ReadLines("../../../inputDay2.txt"))
+            {
+                totalScore += scoreMap[line[2]] + winMap[line];
+            }
+
+            Console.WriteLine("output: " + totalScore);
+        }
+    }
+}
