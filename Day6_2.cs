5e48717ae8022e2e47164ed5e3d5b5fb108a4888
pandakraut <75352338+pandakraut@users.noreply.github.com>
2022-12-06 01:05:42 -0500

    Add files via upload

---
+++ Day6_2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2022
+{
+    class day6_2
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            LinkedList<char> list = new LinkedList<char>();
+            List<char> uniqueList = new List<char>();  
+            foreach (string line in File.ReadLines("../../../inputDay6.txt"))
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    list.AddLast(line[i]);
+                    if (list.Count == 14)
+                    {
+                        foreach (char u in list)
+                        {
+                            if (!uniqueList.Contains(u))
+                            {
+                                uniqueList.Add(u);
+                            }
+                        }
+                        if (uniqueList.Count == 14)
+                        {
+                            total = i+1;
+                            break;
+                        }
+                        else
+                        {
+                            uniqueList.Clear();
+                            list.RemoveFirst();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("output: " + total);
+        }
+    }
+}
